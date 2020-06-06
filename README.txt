72bf3aacc6e8a863dd21ac1bc2cf37bd35ac9683
harry <h.a.vanness@gmail.com>
2020-06-07 01:19:10 +1000

    documentation

--- README.txt
+++ README.txt
@@ -1,3 +1,1 @@
-Run a server from the terminal with:
-python3 -m http.server
-and go to http://localhost:8000
+jk, read the comments in /js/main.js
--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+/********************
+
+Hey grant,
+
+Here's some webgl stuff for you to play with.
+The code is split up like this:
+1. asset_urls and image_urls are URLs of OBJ files, image files, and shader files that will be downloaded into the page.
+2. Application specific variables and updating logic: Currently all this app does is rotate a camera!
+3. Rendering. Currently just renders a screen. See the render_screen() comments.
+4. The "main" function:
+   - Loads the GPU with your model data and textures
+   - Compiles the shaders and gives you control of the shader parameters
+   - And finally starts the 60fps main_loop that updates and renders and updates and renders etc.
+
+Run a server from the terminal with:
+python3 -m http.server
+and go to http://localhost:8000
+
+To get your feet wet:
+- Replace /img/screen.png
+- Replace /obj/screen.obj
+- Change camera_translation and camera_rotation
+- Add an object
+  - add to asset_urls
+  - create render_another_object() using render_screen() as a template
+  - add render_another_object() to the render() function
+  - load the object to the GPU in main() by copying the line for loading the screen to the GPU
+- Edit the basic shader (/shaders/basic.frag and /shaders/basic.vert)
+- Add a shader
+
+GOODLUCKHAVEFUN!
+
+Harry
+
+*********************/
+
+
 /*
  *
  * Declare OBJ, shaders and textures
@@ -27,11 +64,20 @@ let prev_timestamp = null
 let total_time = 0
 const TARGET_FRAME_TIME = 1000/60
 let basic_shader_program = null
+let basic_a_pos = null
+let basic_a_normal = null
+let basic_a_uv = null
 let basic_u_model_view_matrix = null
+let basic_u_projection_matrix = null
 let basic_u_sampler = null
 
 
-let ry = 0
+/*
+ *
+ * Application variables
+ *
+ */
+let camera_rotation_y = 0
 const camera_translation = [
   1, 0, 0, 0,
   0, 1, 0, 0,
@@ -47,35 +93,50 @@ const camera_rotation = [
 
 
 function update () {
-  ry += 0.01
-
-  camera_rotation[0] = Math.cos(ry)
-  camera_rotation[2] = Math.sin(ry)
-  camera_rotation[8] = -Math.sin(ry)
-  camera_rotation[10] = Math.cos(ry)
+  /*
+   * Update stuff at 60fps
+   */
+  camera_rotation_y = camera_rotation_y + 0.01
+  camera_rotation[0] = Math.cos(camera_rotation_y)
+  camera_rotation[2] = Math.sin(camera_rotation_y)
+  camera_rotation[8] = -Math.sin(camera_rotation_y)
+  camera_rotation[10] = Math.cos(camera_rotation_y)
 }
 
-function render () {
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+/*
+ * Use this as a template for rendering things.
+ * 1. Choose the shader
+ * 2. Send the shader the camera matrix and screen texture
+ * 3. Send the shader the vertex indices, and draw
+ */
+function render_screen () {
   gl.useProgram(basic_shader_program)
 
   gl.uniformMatrix4fv(basic_u_model_view_matrix, false, matrix_mult_4(camera_translation, camera_rotation))
+  gl.uniform1i(basic_u_sampler, model_buffers.screen.texture_id)
 
-  const keys = Object.keys(model_buffers)
-  for (let i=0; i<keys.length; i++) {
-    const k = keys[i]
-    gl.uniform1i(basic_u_sampler, model_buffers[k].texture_id)
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model_buffers[k].indices)
-    gl.drawElements(gl.TRIANGLES, model_buffers[k].num_indices, gl.UNSIGNED_SHORT, 0)
-  }
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model_buffers.screen.indices)
+  gl.drawElements(gl.TRIANGLES, model_buffers.screen.num_indices, gl.UNSIGNED_SHORT, 0)
 }
+
+
+function render () {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+  render_screen()
+  // render_chair()
+  // render_window()
+  // render_spider()
+  // render_lady()
+}
+
 
 function init_gl () {
   canvas = document.createElement('canvas')
   document.body.appendChild(canvas)
   canvas.width = window.devicePixelRatio * window.innerWidth
   canvas.height = window.devicePixelRatio * window.innerHeight
-
   gl = canvas.getContext('webgl')
   gl.clearColor(0,0,0,1)
   gl.enable(gl.DEPTH_TEST)
@@ -90,22 +151,22 @@ function main_loop (timestamp) {
   const frame_time = timestamp - prev_timestamp
   prev_timestamp = timestamp
   total_time += frame_time
-
   while (total_time >= TARGET_FRAME_TIME) {
     update()
     render()
     total_time -= TARGET_FRAME_TIME
   }
-
   window.requestAnimationFrame(main_loop)
 }
+
 
 function main () {
   init_gl(canvas, gl)
 
+
   /*
    *
-   * Load OBJ and textures
+   * Load OBJ and textures onto the GPU
    *
    */
 
@@ -115,25 +176,24 @@ function main () {
 
   /*
    *
-   * Initialise shaders
+   * Compile shaders and get control of shader variables
    *
    */
 
   basic_shader_program = create_shader_program(gl, assets.basic_vertex, assets.basic_fragment)
-
   gl.useProgram(basic_shader_program)
 
-  const basic_a_pos = gl.getAttribLocation(basic_shader_program, 'a_pos')
+  basic_a_pos = gl.getAttribLocation(basic_shader_program, 'a_pos')
   gl.bindBuffer(gl.ARRAY_BUFFER, model_buffers.screen.vertices)
   gl.enableVertexAttribArray(basic_a_pos)
   gl.vertexAttribPointer(basic_a_pos, 3, gl.FLOAT, false, 0, 0)
 
-  const basic_a_normal = gl.getAttribLocation(basic_shader_program, 'a_normal')
+  basic_a_normal = gl.getAttribLocation(basic_shader_program, 'a_normal')
   gl.bindBuffer(gl.ARRAY_BUFFER, model_buffers.screen.normals)
   gl.enableVertexAttribArray(basic_a_normal)
   gl.vertexAttribPointer(basic_a_normal, 3, gl.FLOAT, false, 0, 0)
 
-  const basic_a_uv = gl.getAttribLocation(basic_shader_program, 'a_uv')
+  basic_a_uv = gl.getAttribLocation(basic_shader_program, 'a_uv')
   gl.bindBuffer(gl.ARRAY_BUFFER, model_buffers.screen.uvs)
   gl.vertexAttribPointer(basic_a_uv, 2, gl.FLOAT, false, 0, 0)
   gl.enableVertexAttribArray(basic_a_uv)
@@ -141,7 +201,7 @@ function main () {
   basic_u_model_view_matrix = gl.getUniformLocation(basic_shader_program, 'u_model_view_matrix')
   basic_u_sampler = gl.getUniformLocation(basic_shader_program, 'u_sampler')
 
-  const basic_u_projection_matrix = gl.getUniformLocation(basic_shader_program, 'u_projection_matrix')
+  basic_u_projection_matrix = gl.getUniformLocation(basic_shader_program, 'u_projection_matrix')
   const aspect = window.innerWidth / window.innerHeight
   const fov = Math.PI/4
   const near = 1
@@ -152,6 +212,7 @@ function main () {
     0, 0, (near + far)/(near - far), -1,
     0, 0, 2*near*far/(near - far), 0,
   ]))
+
 
   window.requestAnimationFrame(main_loop)
 }
